b02637c3cd9c87d66dc8e79146ffcbf958f2f5df
DoTheG95 <104708516+DoTheG95@users.noreply.github.com>
2023-03-23 22:41:19 +1300

    created Square component

---
+++ src/Board.js
@@ -0,0 +1,37 @@
+import react from 'react';
+
+function Square({ value }){
+    function handleClick(){
+        console.log('clicked');    
+    }
+    return (
+        <button 
+            className='square'
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    )
+}
+
+export default function Board() {
+    return (
+    <>
+    <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+    </div>
+    </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Square from './Square';
 import reportWebVitals from './reportWebVitals';
+import Board from './Board';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Square />
+    <Board />
   </React.StrictMode>
 );
 
