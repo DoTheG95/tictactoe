6311c7304d85a5b158f8047d50a2e1d0b905c69c
DoTheG95 <104708516+DoTheG95@users.noreply.github.com>
2023-03-23 22:30:24 +1300

    created board

---
+++ src/Square.js
@@ -0,0 +1,23 @@
+import react from 'react';
+
+export default function Square() {
+    return (
+    <>
+    <div className='board-row'>
+    <button className='square'> x </button>
+    <button className='square'> x </button>
+    <button className='square'> x </button>
+    </div>
+    <div className='board-row'>
+    <button className='square'> x </button>
+    <button className='square'> x </button>
+    <button className='square'> x </button>
+    </div>
+    <div className='board-row'>
+    <button className='square'> x </button>
+    <button className='square'> x </button>
+    <button className='square'> x </button>
+    </div>
+    </>
+    )
+}
--- src/tictactoe.js
+++
@@ -1,5 +0,0 @@
-import react from 'react';
-
-export default function Square() {
-    return <button className='square'> x </button>;
-}
