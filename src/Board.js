100cffc8502eeab39fa0277a16012649628702dd
DoTheG95 <104708516+DoTheG95@users.noreply.github.com>
2023-03-28 21:41:05 +1300

    added a time machine feature

--- src/Board.js
+++ src/Board.js
@@ -11,10 +11,7 @@ function Square({value, onSquareClick}){
     )
 }
 
-export default function Board() {
-    const [xisNext, setXisNext] = useState(true)
-    const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xisNext, squares, onPlay }) {
     function handleClick(i){
         if (squares[i] || CalculateWinner(squares)){
             return;
@@ -26,11 +23,10 @@ export default function Board() {
         else{
         (nextSquares[i] = 'O')
         }
-        setXisNext(!xisNext);
-        setSquares(nextSquares);
-        }
+        onPlay(nextSquares);
+    }
 
-        const winner = CalculateWinner(squares);
+    const winner = CalculateWinner(squares);
     let status;
     //if winner is decided status = winner side
     if (winner){
@@ -42,24 +38,24 @@ export default function Board() {
     }
 
     return (
-    <>
-    <div className="status">{status}</div>
-    <div className='board-row'>
-        <Square value={squares[0]} onSquareClick = {() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick = {() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick = {() => handleClick(2)}/>
-    </div>
-    <div className='board-row'>
-        <Square value={squares[3]} onSquareClick = {() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick = {() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick = {() => handleClick(5)}/>
-    </div>
-    <div className='board-row'>
-        <Square value={squares[6]} onSquareClick = {() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick = {() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick = {() => handleClick(8)}/>
-    </div>
-    </>
+        <>
+        <div className="status">{status}</div>
+        <div className='board-row'>
+            <Square value={squares[0]} onSquareClick = {() => handleClick(0)}/>
+            <Square value={squares[1]} onSquareClick = {() => handleClick(1)}/>
+            <Square value={squares[2]} onSquareClick = {() => handleClick(2)}/>
+        </div>
+        <div className='board-row'>
+            <Square value={squares[3]} onSquareClick = {() => handleClick(3)}/>
+            <Square value={squares[4]} onSquareClick = {() => handleClick(4)}/>
+            <Square value={squares[5]} onSquareClick = {() => handleClick(5)}/>
+        </div>
+        <div className='board-row'>
+            <Square value={squares[6]} onSquareClick = {() => handleClick(6)}/>
+            <Square value={squares[7]} onSquareClick = {() => handleClick(7)}/>
+            <Square value={squares[8]} onSquareClick = {() => handleClick(8)}/>
+        </div>
+        </>
     )
 }
 
@@ -84,4 +80,52 @@ function CalculateWinner(squares) {
         }
     }
     return null;
+}
+
+export default function Game () {
+    //moved hooks to parent so that it can be used in both parent and lower levels
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xisNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+    
+    function handlePlay(nextSquares){
+        const nextHistory = [...history.slice(0,currentMove+1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length -1);
+    }
+
+    function jumpTo(nextMove){
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move)=> {
+        let description;
+        if(move >0){
+            description = 'Go to move #' + move;
+        }
+        else{
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>
+                    {description}
+                </button>
+            </li>
+        )
+    }
+        
+    )
+
+    return(
+        <div className='game'>
+            <div className='game-board'>
+                <Board xisNext={xisNext} squares={currentSquares} onPlay={handlePlay}/>
+            </div>
+            <div className='game-info'>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    )
 }
